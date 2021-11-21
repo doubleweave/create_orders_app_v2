33795620d77ae14e8cd5bb79a21a46d8c59020ae
doubleweave <33968439+doubleweave@users.noreply.github.com>
2021-11-21 17:42:46 +1100

    swagger+nodejs

--- server/app.js
+++ server/app.js
@@ -3,13 +3,13 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 var Formidable = require('formidable')
 var FS = require('fs')
 
 var indexRouter = require('./routes/index')
-// var apiRouter = require('./routes/api');
-// var uploadRouter = require('./routes/upload');
 
 var app = express()
 
@@ -24,23 +24,33 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-// app.use('/api', apiRouter)
-// app.use('/upload', uploadRouter)
 
-// handle token error
-app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).send({ code: 0, msg: 'token error' })
-  } else {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'create orders app',
+    version: '1.0.0',
+    description: 'send form data to api',
+    contact: {
+      name: 'Luke Liu',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:4000',
+      description: 'Development server',
+    },
+  ],
+  schemes: ['https', 'http'],
+}
 
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-  }
-})
+const options = {
+  swaggerDefinition,
+  apis: [`${__dirname}/routes/*.js`],
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,70 +9,178 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-// router.get('/v2/orders', (req, res) => {
+/**
+ * @swagger
+ * /createOrders:
+ *    post:
+ *      summary: Sending form data to target api
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               totalAmount:
+ *                 type: object
+ *                 description: Total Amount of the order
+ *                 properties:
+ *                  amount:
+ *                    type: string
+ *                    description: Amount of the order
+ *                    example: '190.00'
+ *                  currency:
+ *                    type: string
+ *                    description: Currency type (should be EUR)
+ *                    example: 'EUR'
+ *               consumer:
+ *                 type: object
+ *                 description: Total Amount of the order
+ *                 properties:
+ *                  phoneNumber:
+ *                    type: string
+ *                    example: '0400000001'
+ *                  givenNames:
+ *                    type: string
+ *                    description: First name
+ *                    example: 'Joe'
+ *                  surname:
+ *                    type: string
+ *                    description: Last name
+ *                    example: 'Smith'
+ *                  email:
+ *                    type: string
+ *                    description: Consumer's email
+ *                    example: 'test@scalapay.com'
+ *               billing:
+ *                 type: object
+ *                 properties:
+ *                  phoneNumber:
+ *                    type: string
+ *                    example: '0400000000'
+ *                  countryCode:
+ *                    type: string
+ *                    example: 'IT'
+ *                  name:
+ *                    type: string
+ *                    example: 'Joe Consumer'
+ *                  postcode:
+ *                    type: string
+ *                    example: '50056'
+ *                  suburb:
+ *                    type: string
+ *                    example: 'Montelupo Fiorentino'
+ *                  line1:
+ *                    type: string
+ *                    example: 'Via della Rosa, 58'
+ *               shipping:
+ *                 type: object
+ *                 properties:
+ *                  phoneNumber:
+ *                    type: string
+ *                    description: Customer's phone number
+ *                    example: '0400000000'
+ *                  countryCode:
+ *                    type: string
+ *                    description: Two letter code
+ *                    example: 'IT'
+ *                  name:
+ *                    type: string
+ *                    example: 'Joe Consumer'
+ *                  postcode:
+ *                    type: string
+ *                    example: '50056'
+ *                  suburb:
+ *                    type: string
+ *                    example: 'Montelupo Fiorentino'
+ *                  line1:
+ *                    type: string
+ *                    example: 'Via della Rosa, 58'
+ *               items:
+ *                 type: array
+ *                 items:
+ *                  type: object
+ *                  properties:
+ *                    gtin:
+ *                      type: string
+ *                      description: Global Trade Item Number. One of [UPC, EAN, JAN, ISBN, ITF-14]
+ *                      example: '123458791330'
+ *                    quantity:
+ *                      type: number
+ *                      example: 1
+ *                    price:
+ *                      type: object
+ *                      properties:
+ *                        amount:
+ *                          type: string
+ *                          example: '10.00'
+ *                        currency:
+ *                          type: string
+ *                          example: 'EUR'
+ *                    name:
+ *                      type: string
+ *                      example: 'T-Shirt'
+ *                    category:
+ *                      type: string
+ *                      example: 'clothes'
+ *                    sku:
+ *                      type: string
+ *                      description: Stock Keeping Unit code
+ *                      example: '12341234'
+ *                    brand:
+ *                      type: string
+ *                      example: 'TopChoice'
+ *               merchant:
+ *                 type: object
+ *                 properties:
+ *                  redirectCancelUrl:
+ *                    type: string
+ *                    example: 'https://portal.staging.scalapay.com/failure-url'
+ *                  redirectConfirmUrl:
+ *                    type: string
+ *                    example: 'https://portal.staging.scalapay.com/success-url'
+ *               merchantReference:
+ *                 type: string
+ *                 example: 'merchantOrder-1234'
+ *               shippingAmount:
+ *                 type: object
+ *                 properties:
+ *                  amount:
+ *                    type: string
+ *                    example: '10.00'
+ *                  currency:
+ *                    type: string
+ *                    example: 'EUR'
+ *               orderExpiryMilliseconds:
+ *                 type: number
+ *                 example: 600000
+ *      responses:
+ *        200:
+ *          descriptions: return token and checkoutUrl
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  token:
+ *                    type: string
+ *                    description: "Scalapay order unique token"
+ *                    example: "71KH916VPE"
+ *                  expires:
+ *                    type: string
+ *                    description: "Date and time of the order to expire in ISO 8601 format"
+ *                    example: "2020-11-08T13:06:17.858Z"
+ *                  checkoutUrl:
+ *                    type: string
+ *                    description: "Redirect Url to the Scalapay checkout"
+ *                    example: "https://portal.staging.scalapay.com/checkout/71KH916VPE"
+ */
 router.post('/createOrders', (req, res) => {
-  atoken = 'qhtfs87hjnc12kkos '
-
-  console.log('req.body: ', req.body)
-
   const data = req.body
 
-  console.log('data: ', req.body)
-
-  //   const data = {
-  //     totalAmount: {
-  //       amount: '190.00',
-  //       currency: 'EUR',
-  //     },
-  //     consumer: {
-  //       givenNames: 'Joe',
-  //       phoneNumber: '0400000001',
-  //       surname: 'Li',
-  //       email: 'test@scalapay.com',
-  //     },
-  //     billing: {
-  //       phoneNumber: '0400000000',
-  //       countryCode: 'IT',
-  //       name: 'Joe Consumer',
-  //       postcode: '50056',
-  //       suburb: 'Montelupo Fiorentino',
-  //       line1: 'Via della Rosa, 58',
-  //     },
-  //     shipping: {
-  //       phoneNumber: '0400000000',
-  //       countryCode: 'IT',
-  //       name: 'Joe Consumer',
-  //       postcode: '50056',
-  //       suburb: 'Montelupo Fiorentino',
-  //       line1: 'Via della Rosa, 58',
-  //     },
-  //     items: [
-  //       {
-  //         price: {
-  //           amount: '10.00',
-  //           currency: 'EUR',
-  //         },
-  //         quantity: 1,
-  //         gtin: '123458791330',
-  //         name: 'T-Shirt',
-  //         category: 'clothes',
-  //         sku: '12341234',
-  //         brand: 'TopChoice',
-  //       },
-  //     ],
-  //     merchant: {
-  //       redirectCancelUrl: 'https://portal.staging.scalapay.com/failure-url',
-  //       redirectConfirmUrl: 'https://portal.staging.scalapay.com/success-url',
-  //     },
-  //     shippingAmount: {
-  //       amount: '10.00',
-  //       currency: 'EUR',
-  //     },
-  //     orderExpiryMilliseconds: 600000,
-  //     merchantReference: 'merchantOrder-1234',
-  //   }
-
-  axios.defaults.headers.common = { Authorization: `bearer ${atoken}` }
+  axios.defaults.headers.common = {
+    Authorization: `bearer ${process.env.ACCESS_TOKEN}`,
+  }
   axios
     .post('https://staging.api.scalapay.com/v2/orders', data)
     .then((result) => {
